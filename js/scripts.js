ef5d3f496eda87a33b395e0e96962277f375e690
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2023-06-09 12:49:16 -0300

    em andamento erro linha 6

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Seleção de Elementos
+const generatePasswordButton = document .querySelector("generate-password");
+const generatePasswordElement = document.querySelector("generated-password");
+
+//Funções
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste");
+});
+//Eventos
