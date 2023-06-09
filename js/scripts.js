7d5c9d52f78783d51fdbd19c4364d62cce960fe6
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2023-06-09 17:35:32 -0300

    em andamento...

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,27 @@ const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
 //Funções
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=/,.!@#$%&+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// O console serve para testa o código: console.log(getSymbol());
+// sendo assim serve para vê se está funcionando.
+
+//Eventos
 generatePasswordButton.addEventListener("click", () => {
     console.log("teste");
 });
-//Eventos
